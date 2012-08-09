91a60ce2161e4b0c76a8cab1cf912f972fcbe990
luo <i@liluo.org>
2012-08-10 01:19:19 +0800

    update setup.py with generate egg

--- setup.py
+++ setup.py
@@ -8,13 +8,15 @@ setup(
     version = '0.0.1',
     keywords = ('Python', 'OAuth2', 'Douban', 'GitHub', 'Weibo'),
     description = 'Python OAuth2',
+    long_description = 'Python OAuth2',
     license = 'MIT License',
 
     url = 'https://github.com/liluo/py-oauth2',
     author = 'liluo',
-    autho_email = 'i@liluo.org',
+    author_email = 'i@liluo.org',
 
     packages = find_packages(),
+    platforms = 'Independant',
     install_requires = ['httplib2'],
 
 )
