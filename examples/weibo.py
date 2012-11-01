14b204a5eec5ee092681be15f4d50d9d30bbcd21
liluo <i@liluo.org>
2012-11-01 11:33:09 +0800

    updated example for weibo

--- examples/weibo.py
+++ examples/weibo.py
@@ -16,15 +16,15 @@ print authorize_url
 
 code = raw_input('Enter the verification code and hit ENTER when you\'re done:')
 code = code.strip()
-access_token = client.auth_code.get_token(code, redirect_uri=CALLBACK)
+access_token = client.auth_code.get_token(code, redirect_uri=CALLBACK, header_format='OAuth2 %s')
 print 'token', access_token.headers
 
-ret = access_token.get('/2/statuses/public_timeline.json', access_token=access_token.token)
+ret = access_token.get('/2/statuses/public_timeline.json')
 print '-' * 80
 print 'get public timeline' 
 print ret.body
 
 print '-' * 80
 print 'post miniblog...'
-ret = access_token.post('/2/statuses/update.json', status='now', access_token=access_token.token)
+ret = access_token.post('/2/statuses/update.json', status='now')
 print ret.body
